9ce03af7038aacfd892079783266545b2184ad97
bemmanue <myaushenka@gmail.com>
2023-04-07 09:46:41 +0300

    update migrations

---
+++ migrations/000001_create_all.down.sql
@@ -0,0 +1,6 @@
+drop table comments;
+drop table images;
+drop table likes;
+drop table posts;
+drop table users;
+drop table verify_codes;
--- migrations/20230314141205_create_users.up.sql
+++
@@ -1,6 +0,0 @@
-create table users (
-    user_id bigserial not null primary key,
-    username varchar(30) not null unique,
-    email varchar(60) not null unique,
-    encrypted_password varchar not null
-)
